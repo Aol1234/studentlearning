22f6f3faa0e40e6caa031bec83eb57ae14c80ded
Aidan <aidan@fixedpointcode.com>
2019-03-05 18:45:07 +0000

    Statistics for MCQ

---
+++ src/components/MultipleChoiceQuestions/Statistics.vue
@@ -0,0 +1,31 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import {eventHub} from '@/EventHub'
+export default {
+  name: 'Statistics',
+  props: ['answer'],
+  watch: {
+    answer: {
+      handler: function (Answer) {
+        eventHub.$emit('Update Answer', Answer)
+        eventHub.$emit('Update Answer Count', Answer['Id'])
+        eventHub.$emit('Update Answer time', Answer)
+      },
+      deep: true
+    }
+  },
+  data () {
+    return {
+      timeSinceLastQuestionAnswered: 0 // time since last question answered
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
