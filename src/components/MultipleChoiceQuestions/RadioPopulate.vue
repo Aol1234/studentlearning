426f701f9400687232194b2f2985d1d3104a5ad8
Aidan <aidan@fixedpointcode.com>
2019-03-06 18:48:19 +0000

    Refactored RadioPopulate

--- src/components/MultipleChoiceQuestions/RadioPopulate.vue
+++ src/components/MultipleChoiceQuestions/RadioPopulate.vue
@@ -1,50 +1,52 @@
 <template>
   <div>
-    {{selected}}
-    <div v-if="rendering === true" v-for="(n, i) in this.questions" :key="i">
+    {{Answers}}
+    <div v-if="rendering === true" v-for="(n, i) in this.numberOfQuestions" :key="i">
      {{question[i]}}
       <b-form-group>
-        <b-form-radio-group v-model= selected[i]
-                            :options=questionnaire[i]
-                            :name=names[i]>
+        <b-form-radio-group v-model="selectedAnswers[i]['result']"
+                            :options="questionnaire[i]"
+                            :name="names[i]">
         </b-form-radio-group>
       </b-form-group>
     </div>
     <button v-on:click="populate">Populate</button>
     <button v-on:click="result">Result</button>
+      <p>List of people:</p>
+      <Statistics v-for="(answer, index) in selectedAnswers" :key="index.id" :answer="answer" :Answers="Answers"></Statistics>
   </div>
 </template>
 <script>
+import {sessionToken, api} from '../LoginPage'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import axios from 'axios'
-const api = 'http://localhost:8002/'
+import {eventHub} from '@/EventHub'
+import Statistics from '@/components/MultipleChoiceQuestions/Statistics'
+// import * as Duration from 'duration-js'
 export default {
   name: 'RadioPopulate',
+  components: {Statistics},
   data () {
     return {
-      questions: 0, // json.numberOfQuestions // populated with number of questions
-      rendering: false,
-      answers: [], // populated with answers
-      selected: [], // populated with selected radio buttons
+      numberOfQuestions: 0, // json.numberOfQuestions // populated with number of questions
+      rendering: false, // Prevent Rending until data evaluated,
+      timer: null, // Counts time
+      elapsed: null,
       names: [], // populated with identifiers of questions
-      questionnaire: [],
+      questionnaire: [], // Holds arrays of Answers
       questionnaireInfo: [],
-      question: [],
-      response: []
+      question: [], // Holds array of Questions
+      response: [], // REST response
+      results: [], // selected is reformatted here before sending to server selected{id:0, val:1, time:12.01'secs'} -> results{result:1, time:12.01}
+      selectedAnswers: [], // Populated with selected radio buttons
+      Answers: [],
+      AnswerLength: [], // Ad-hoc hack to get answer length
+      McqId: 0,
+      UserId: 0 // Placeholder, swapped server side
     }
   },
   methods: {
-    getNumberOfQuestions () {
-      let names = []
-      let questions = this.questionnaire.length
-      this.questions = questions
-      for (let question in questions) {
-        names.push(question)
-        this.selected[questions].push('')
-      }
-      this.names = names
-    },
     getAnswers () {
       let questions = this.questionnaire
       for (let question in questions) {
@@ -55,15 +57,33 @@ export default {
         }
       }
     },
+    // Formatting
+    formatSelected () {
+      for (let question in this.question) {
+        this.selectedAnswers.push({Id: question, result: -1, total: this.AnswerLength[question], time: -1, changes: 0})
+        this.Answers.push({Id: question, result: -1, total: this.AnswerLength[question], time: -1, changes: 0})
+      }
+    },
     populate () {
-      this.getNumberOfQuestions()
+      this.numberOfQuestions = this.question.length
+      this.formatSelected()
       this.getAnswers()
+      this.timer = performance.now()
       this.rendering = true // Allows questions to render after preceding methods
     },
     result () {
-      let body = {results: ['test']}
-      console.log(this.selected)
-      axios.post(api + ':ID/:mcqID/result', JSON.stringify(body))
+      let result = {
+        'McqId': this.McqId,
+        'UserId': this.UserId, // Placeholder value
+        'McqQuestionResult': this.Answers
+      }
+      console.log(result)
+      axios({
+        method: 'post',
+        url: api + ':ID/:mcqID/result',
+        data: JSON.stringify(result),
+        headers: { 'Authorization': 'Bearer  ' + sessionToken }
+      })
         .then((response) => {
           this.response = response.data
           console.log('Response of results', this.response)
@@ -72,24 +92,49 @@ export default {
           console.log(error)
           this.errored = true
         })
+    },
+    getTime (Answer) {
+      if (this.elapsed == null) { // First Answer
+        this.elapsed = this.timer // Time from start
+        return Math.floor(this.elapsed) * 1000000
+      } else if (this.Answers[Answer['Id']]['time'] === -1) { // First time this question answered
+        this.timer = performance.now() - this.elapsed
+        this.elapsed = performance.now()
+        return Math.floor(this.timer) * 1000000
+      } else { // Updated Answer
+        this.timer = performance.now() - this.elapsed
+        this.elapsed = performance.now()
+        return Math.floor(this.Answers[Answer['Id']]['time'] + (this.timer * 1000000))
+      }
+    },
+    getMCQ () {
+      axios({
+        method: 'post',
+        url: api + 'post',
+        // data: JSON.stringify(result),
+        headers: { 'Authorization': 'Bearer  ' + sessionToken }
+      })
+        .then((response) => {
+          this.response = response.data['Value']
+          let questions = response.data['Value']['McqQuestions']
+          // this.questionnaireInfo = this.response['Value']['MCQ_ID']
+          for (let question in questions) {
+            this.question.push(questions[question]['Question']) // Single Array
+            this.AnswerLength.push(questions[question]['Answers'].length)
+            this.questionnaire.push(questions[question]['Answers']) // Multiple Arrays
+          }
+        })
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
     }
   },
   created () {
-    // let body = {object_one: 'valueOne', object_two: 'valueTwo', array_one: {a: 'one', b: 'two'}}
-    axios.post(api + 'post')
-      .then((response) => {
-        this.response = response.data
-        let questions = response.data['Questions']
-        this.questionnaireInfo = this.response['Identifier']
-        for (let question in questions) {
-          this.question.push(questions[question]['Question'])
-          this.questionnaire.push(questions[question]['Answers'])
-        }
-      })
-      .catch(error => {
-        console.log(error)
-        this.errored = true
-      })
+    this.getMCQ()
+    eventHub.$on('Update Answer Count', Id => { this.Answers[Id]['changes'] += 1 })
+    eventHub.$on('Update Answer', Answer => { this.Answers[Answer['Id']]['result'] = Answer['result'] })
+    eventHub.$on('Update Answer time', Answer => { this.Answers[Answer['Id']]['time'] = this.getTime(Answer) })
   }
 }
 </script>
